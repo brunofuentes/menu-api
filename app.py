72a472fcdfdb081ccd83f36c4e80bf50cf5a38bc
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-08-16 20:31:12 +0200

    fix

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    app.config.from_object('config')
+    app.config.from_object(Config)
     setup_db(app)
     migrate.init_app(app, db)
     admin = Admin(app)
