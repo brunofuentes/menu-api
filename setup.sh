22f53b3d77d3e566e8b2ea7b91685e057f78159b
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-08-18 10:53:15 +0200

    update database-url

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-export DATABASE_URL='postgresql://Bruno@localhost:5432/menu-db-v1'
+export DATABASE_URL="postgres://dagalqtokkdhgp:484c6a20c8c3dc59ca6b6fc5e77b26b8bad1ac595eaa7a4dac4de5be74825dae@ec2-54-85-56-210.compute-1.amazonaws.com:5432/d3i2dqabk20hdu"
