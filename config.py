a1f0633a06212c685b5cf8300584bf01687070ba
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-08-18 16:13:33 +0200

    delete database link

--- config.py
+++ config.py
@@ -4,7 +4,4 @@ import os
 class Config:
     basedir = os.path.abspath(os.path.dirname(__file__))
     DEBUG = True
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost:5432/menu-db-v1'
-    SQLALCHEMY_DATABASE_URI = 'postgres://dagalqtokkdhgp:484c6a20c8c3dc59ca6b6fc5e77b26b8bad1ac595eaa7a4dac4de5be74825dae@ec2-54-85-56-210.compute-1.amazonaws.com:5432/d3i2dqabk20hdu'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(32)
