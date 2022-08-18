d8f56e4d88f42694ce00b644c6af4fd3afbace21
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-08-18 11:09:07 +0200

    fix database-path

--- models.py
+++ models.py
@@ -12,6 +12,9 @@ db = SQLAlchemy()
 
 
 def setup_db(app, database_path=database_path):
+    if database_path.startswith("postgres://"):
+        database_path = database_path.replace(
+            "postgres://", "postgresql://", 1)
     app.config['SQLALCHEMY_DATABASE_URI'] = database_path
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
